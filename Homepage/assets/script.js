d9afc629353b4caf9388cdb57ebfaef6bd8ead2c
nuadabr <linchidactyl@gmail.com>
2018-10-12 17:11:51 +1100

    more max web

---
+++ Homepage/assets/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('img-change').mouseenter(function() {
+        $('.index-container').addClass('img-update');
+    });
+
+    $('img-change').mouseleave(function() {
+        $('.index-container').removeClass('img-update');
+    });
+
+    $('img-change').mouseenter(function() {
+        $('.index-container').addClass('img-update');
+    });
+
+    $('img-change').mouseleave(function() {
+        $('.index-container').removeClass('img-update');
+    });
+})
---
+++ maximum-website/assets/js/essay.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.e-img').mouseenter(function() {
+        $('.essaytext-container').addClass('show');
+    });
+
+    $('.e-img').mouseleave(function() {
+        $('.essaytext-container').removeClass('show');
+    });
+});
+
+
+
+
